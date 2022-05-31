ebaea6627d84e02f03b5749f42eb5a532a0f3f24
Rama Setyawan <rmstwn@gmail.com>
2022-05-31 15:49:18 +0700

    Add overlay

--- TOF_GUI/Form1.Designer.vb
+++ TOF_GUI/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,22 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
         Me.PictureBox1.Location = New System.Drawing.Point(-2, -1)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(1075, 651)
+        Me.PictureBox1.Size = New System.Drawing.Size(1076, 651)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
@@ -43,12 +46,25 @@ Partial Class Form1
         Me.TextBox1.Size = New System.Drawing.Size(125, 27)
         Me.TextBox1.TabIndex = 1
         '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Location = New System.Drawing.Point(-2, -1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1076, 651)
+        Me.Panel1.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1072, 648)
         Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "TOF GUI"
@@ -60,4 +76,6 @@ Partial Class Form1
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Panel1 As Panel
 End Class
--- TOF_GUI/Form1.vb
+++ TOF_GUI/Form1.vb
@@ -4,30 +4,69 @@ Public Class Form1
     Dim myPen As New Pen(Color.Green, 2)
     Dim g As Graphics
 
+    Private Img1 As Image = Image.FromFile("D:\Kuliah\ROISC\TOF Sensor\GUI\projects\TOF_GUI\TOF_GUI\Resources\clear.png")
+
     Dim counter As Integer = 1
 
-    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
-        PictureBox1.Size = New Size(Me.Width, Me.Height)
-        PictureBox1.Image = Nothing
+    'Private Sub DrawCircle(ByVal gr As Graphics, ByVal ratio As Integer)
 
-        PictureBox1.Refresh()
+    '    Dim radius As Integer = 100
+    '    Dim x As Integer
+    '    Dim y As Integer
 
-        g = PictureBox1.CreateGraphics()
+    '    Dim i As Integer
+    '    Dim j As Integer = 10
 
-        DrawCircle(g, counter)
-    End Sub
+    '    For i = 20 To 900 Step 40
+    '        radius = i * ratio
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        PictureBox1.Size = New Size(Me.Width, Me.Height)
+    '        x = Convert.ToInt32(PictureBox1.Size.Width \ 2) - radius
+    '        y = Convert.ToInt32(PictureBox1.Size.Height \ 2) - radius
 
-        PictureBox1.Image = Nothing
-        PictureBox1.Refresh()
+    '        gr.DrawEllipse(myPen, x, y,
+    '                               radius * 2, radius * 2)
 
-        g = PictureBox1.CreateGraphics()
+    '        If j >= 100 Then
+    '            'Label Horizontal
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, x - 32, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, (Convert.ToInt32(PictureBox1.Size.Width \ 2) + radius) - 1, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
 
-        DrawCircle(g, 1)
+    '            'Label Vertical
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 16, y - 17)
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 16, Convert.ToInt32(PictureBox1.Size.Height \ 2) + radius)
+    '        Else
+    '            'Label Horizontal
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, x - 21, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, (Convert.ToInt32(PictureBox1.Size.Width \ 2) + radius), Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
 
-    End Sub
+    '            'Label Vertical
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 11, y - 17)
+    '            g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 11, Convert.ToInt32(PictureBox1.Size.Height \ 2) + radius)
+    '        End If
+    '        j += 20
+    '    Next
+
+    '    Dim RealDistance(80) As Double
+    '    Dim Distance(80) As Double
+    '    Dim Angle As Double
+
+    '    For i = 0 To 79
+    '        RealDistance(i) = 90
+
+    '        Distance(i) = (RealDistance(i) * 2)
+
+    '        Angle = (i * 4.5) - 360
+
+    '        Dim Point As New Point(Convert.ToInt32(PictureBox1.Size.Width \ 2) + Distance(i) * Math.Cos(Angle * Math.PI / 180), Convert.ToInt32(PictureBox1.Size.Height \ 2) + Distance(i) * Math.Sin(Angle * Math.PI / 180))
+
+    '        DrawPoint(Point, gr, Color.Red)
+    '    Next
+
+    '    'PictureBox1.Invalidate()
+    '    'myPen.Dispose()
+
+    '    gr.Dispose()
+    'End Sub
 
     Private Sub DrawCircle(ByVal gr As Graphics, ByVal ratio As Integer)
 
@@ -38,41 +77,44 @@ Public Class Form1
         Dim i As Integer
         Dim j As Integer = 10
 
+        Dim thmb = New Bitmap(CInt(PictureBox1.Width), CInt(PictureBox1.Height))
+        Dim graph As Graphics = Graphics.FromImage(thmb)
+
+        graph.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        graph.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+
         For i = 20 To 900 Step 40
             radius = i * ratio
 
             x = Convert.ToInt32(PictureBox1.Size.Width \ 2) - radius
             y = Convert.ToInt32(PictureBox1.Size.Height \ 2) - radius
 
-            gr.DrawEllipse(myPen, x, y,
+            graph.DrawEllipse(myPen, x, y,
                                    radius * 2, radius * 2)
 
             If j >= 100 Then
                 'Label Horizontal
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, x - 32, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, (Convert.ToInt32(PictureBox1.Size.Width \ 2) + radius) - 1, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, x - 32, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, (Convert.ToInt32(PictureBox1.Size.Width \ 2) + radius) - 1, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
 
                 'Label Vertical
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 16, y - 17)
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 16, Convert.ToInt32(PictureBox1.Size.Height \ 2) + radius)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 16, y - 17)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 16, Convert.ToInt32(PictureBox1.Size.Height \ 2) + radius)
             Else
                 'Label Horizontal
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, x - 21, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, (Convert.ToInt32(PictureBox1.Size.Width \ 2) + radius), Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, x - 21, Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, (Convert.ToInt32(PictureBox1.Size.Width \ 2) + radius), Convert.ToInt32(PictureBox1.Size.Height \ 2) - 10)
 
                 'Label Vertical
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 11, y - 17)
-                g.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 11, Convert.ToInt32(PictureBox1.Size.Height \ 2) + radius)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 11, y - 17)
+                graph.DrawString(Convert.ToString(j), New Font("Arial", 11), Brushes.Green, Convert.ToInt32(PictureBox1.Size.Width \ 2) - 11, Convert.ToInt32(PictureBox1.Size.Height \ 2) + radius)
             End If
-
             j += 20
-
         Next
 
         Dim RealDistance(80) As Double
         Dim Distance(80) As Double
         Dim Angle As Double
-
 
         For i = 0 To 79
             RealDistance(i) = 90
@@ -83,9 +125,10 @@ Public Class Form1
 
             Dim Point As New Point(Convert.ToInt32(PictureBox1.Size.Width \ 2) + Distance(i) * Math.Cos(Angle * Math.PI / 180), Convert.ToInt32(PictureBox1.Size.Height \ 2) + Distance(i) * Math.Sin(Angle * Math.PI / 180))
 
-            DrawPoint(Point, gr, Color.Red)
+            DrawPoint(Point, graph, Color.Red)
         Next
 
+        PictureBox1.Image = thmb
         'PictureBox1.Invalidate()
         'myPen.Dispose()
 
@@ -98,6 +141,31 @@ Public Class Form1
         Using brsh As New SolidBrush(clr)
             G.FillEllipse(brsh, rc)
         End Using
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.Size = New Size(Me.Width, Me.Height)
+        PictureBox1.Image = Nothing
+        PictureBox1.Refresh()
+
+        g = PictureBox1.CreateGraphics()
+
+        DrawCircle(g, 1)
+
+        Panel1.BackColor = Color.Transparent
+        Panel1.Parent = PictureBox1
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        PictureBox1.Size = New Size(Me.Width, Me.Height)
+        PictureBox1.Image = Nothing
+
+        PictureBox1.Refresh()
+
+        g = PictureBox1.CreateGraphics()
+
+        DrawCircle(g, counter)
+
     End Sub
 
     Private Sub PictureBox1_MouseWheel(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseWheel
@@ -129,5 +197,4 @@ Public Class Form1
             TextBox1.Text = counter
         End If
     End Sub
-
 End Class
